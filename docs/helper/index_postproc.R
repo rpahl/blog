4a638f5c4687e1e0da3304a1a14381c4dff4e9d8
Roman Pahl <roman.pahl@gmail.com>
2024-10-07 15:27:32 +0200

    add blog post 'Nested unit tests with testthat'

--- docs/helper/index_postproc.R
+++ docs/helper/index_postproc.R
@@ -1,11 +1,14 @@
 
-repair_index_file = function(file = "index.xml")
+repair_links = function(file = "index.xml")
 {
     root = dirname(file)
-    lines = readLines("index.xml")
+    lines = readLines(file)
     postdirs = basename(list.dirs(file.path(root, "posts")))[-1]
 
     iBroken = grep(pattern = "r-some-blog//", fixed = TRUE, x = lines)
+    if (length(iBroken) == 0)
+        return()
+
     lines_with_bad_link = lines[iBroken]
 
     repair_bad_link = function(x) {
@@ -34,4 +37,50 @@ repair_index_file = function(file = "index.xml")
     writeLines(lines, file)
 }
 
+
+read_Rmd_header = function(file)
+{
+    lines = readLines(file)
+    dash3 = grep(pattern = "---", x = lines, fixed = TRUE)
+    iStart = dash3[1] + 1
+    iEnd = dash3[2] - 1
+    yaml::read_yaml(text = lines[iStart:iEnd])
+}
+
+
+add_description_from_rmd_to_index_file = function(rmdFile, indexFile)
+{
+    rmdHeader = read_Rmd_header(rmdFile)
+    index = readLines(file)
+    len = length(index)
+
+    # Find description tag in index file
+    titleElement = paste0("<title>", rmdHeader[["title"]], "</title>")
+    titleLineNumber = grep(titleElement, index, fixed = TRUE)
+
+    ii = titleLineNumber +
+        grep("<description>", index[titleLineNumber:len], fixed = TRUE)[1] - 1
+
+    # Add description from Rmd file
+    updatedIndex = c(index[1:ii], rmdHeader[["description"]], index[(ii+1):len])
+    writeLines(updatedIndex, file)
+}
+
+
+
+postproc_index_file = function(file = "index.xml")
+{
+    repair_links(file)
+
+    wd = dirname(file)
+    posts_folder = file.path(wd, "..", "_posts")
+
+    rmdFiles = list.files(posts_folder,
+                          recursive = TRUE,
+                          full.names = TRUE,
+                          pattern = "*.Rmd")
+
+    for (rmdFile in rmdFiles)
+        add_description_from_rmd_to_index_file(rmdFile, indexFile = file)
+}
 
